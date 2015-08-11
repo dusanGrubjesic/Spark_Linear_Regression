58b982d523ef3ec54577e6ac2345446f227d5ffb
Dusan Grubjesic <grubjesic.dusan@gmail.com>
2015-08-11 20:09:15 +0200

    added valuation

--- ClickRate.py
+++ ClickRate.py
@@ -5,8 +5,8 @@ from pyspark.mllib.linalg import SparseVector
 import numpy as np
 from pyspark.mllib.regression import LabeledPoint
 
-dacsample = "/home/dusan/Documents/dac_sample.txt"  # Should be some file on your system
-sc = SparkContext("local", "Simple App")
+dacsample = "/home/dusan/Spark_Linear_Regression/dac_sample.txt"  # Should be some file on your system
+sc = SparkContext("local[4]", "Simple App")
 
 dacsample = open(dacsample)
 dacData = [unicode(x.replace('\n', '').replace('\t', ',')) for x in dacsample]
@@ -98,6 +98,85 @@ includeIntercept = True
 
 model0 = LogisticRegressionWithSGD.train(data=OHETrainData, iterations=numIters, step=stepSize,regParam=regParam, regType=regType, intercept=includeIntercept)
 sortedWeights = sorted(model0.weights)
+
+from math import log
+def computeLogLoss(p, y):
+   
+    epsilon = 10e-12
+    if (p==0):
+      p = p + epsilon
+    elif (p==1):
+      p = p - epsilon
+      
+    if y == 1:
+      z = -log(p)
+    elif y == 0:
+      z = -log(1 - p)
+    return z
+
+
+
+from math import exp #  exp(-t) = e^-t
+def getP(x, w, intercept):
+   
+    
+    rawPrediction = 1 / (1 + exp( (- (w.T.dot(x.toArray())))  + intercept))
+    # Bound the raw prediction value
+    rawPrediction = min(rawPrediction, 20)
+    rawPrediction = max(rawPrediction, -20)
+    return rawPrediction
+
+
+
+def evaluateResults(model, data):
+    
+    tp = data.map(lambda x: (getP(x.features, model.weights ,model.intercept), x.label))
+    answer=tp.map(lambda x:computeLogLoss(x[0],x[1])).mean()
+    return answer
+
+from operator import add
+classOneFracTrain = OHETrainData.map(lambda x : x.label).mean()
+
+logLossTrBase = OHETrainData.map(lambda x:computeLogLoss(classOneFracTrain, x.label)).mean()
+LogLossTrLR0 = evaluateResults(model0, OHETrainData)
+
+print "--------------LogLossTrLR0------"
+print LogLossTrLR0
+print "--------------LogLossTrLR0------"
+
+##print ('OHE Features Train Logloss:\n\tBaseline = {0:.3f}\n\tLogReg = {1:.3f}'
+  ##     .format(logLossTrBase, logLossTrLR0))
+
+logLossValBase = OHEValidationData.map(lambda x:computeLogLoss(classOneFracTrain, x.label)).mean()
+
+numIters = 500
+regType = 'l2'
+includeIntercept = True
+
+# Initialize variables using values from initial model training
+bestModel = None
+bestLogLoss = 1e10
+
+# COMMAND ----------
+
+
+
+stepSizes = (1., 10.)
+regParams = (1e-6, 1e-3)
+for stepSize in stepSizes:
+    for regParam in regParams:
+        model = (LogisticRegressionWithSGD
+                 .train(OHETrainData, numIters, stepSize, regParam=regParam, regType=regType,
+                        intercept=includeIntercept))
+        logLossVa = evaluateResults(model, OHEValidationData)
+        print ('\tstepSize = {0:.1f}, regParam = {1:.0e}: logloss = {2:.3f}'
+               .format(stepSize, regParam, logLossVa))
+        if (logLossVa < bestLogLoss):
+            bestModel = model
+            bestLogLoss = logLossVa
+
+print ('Features Validation Logloss:\n\tBaseline = {0:.3f}\n\tLogReg = {1:.3f}'
+       .format(logLossValBase, bestLogLoss))
 
 sc.stop()
 
---
+++ startCliclRate.sh
@@ -0,0 +1,2 @@
+cd /home/dusan/spark-1.4.1-bin-hadoop2.6/bin
+./spark-submit  /home/dusan/Spark_Linear_Regression/ClickRate.py
